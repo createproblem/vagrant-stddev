f845ff476dd741fb57e5163ade1f7b645b5c9780
createproblem <createproblem@hushmail.com>
2016-02-05 14:26:23 +0100

    init

---
+++ Vagrantfile
@@ -0,0 +1,105 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+vagrant_dir = File.expand_path(File.dirname(__FILE__))
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure(2) do |config|
+  vagrant_version = Vagrant::VERSION.sub(/^v/, '')
+
+  # The most common configuration options are documented and commented below.
+  # For a complete reference, please see the online documentation at
+  # https://docs.vagrantup.com.
+
+  # Every Vagrant development environment requires a box. You can search for
+  # boxes at https://atlas.hashicorp.com/search.
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.provider :virtualbox do |v|
+    v.customize ["modifyvm", :id, "--memory", 1024]
+    v.customize ["modifyvm", :id, "--cpus", 1]
+    v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+
+    # Set the box name in VirtualBox to match the working directory.
+    devm01_pwd = Dir.pwd
+    v.name = File.basename(devm01_pwd)
+  end
+
+  # SSH Agent Forwarding
+  #
+  # Enable agent forwarding on vagrant ssh commands. This allows you to use ssh keys
+  # on your host machine inside the guest. See the manual for `ssh-add`.
+  config.ssh.forward_agent = true
+
+  config.vm.hostname = "devm01"
+
+  # Private Network (default)
+  #
+  # A private network is created by default. This is the IP address through which your
+  # host machine will communicate to the guest. In this default configuration, the virtual
+  # machine will have an IP address of 192.168.50.4 and a virtual network adapter will be
+  # created on your host machine with the IP of 192.168.50.1 as a gateway.
+  #
+  # Access to the guest machine is only available to your local host. To provide access to
+  # other devices, a public network should be configured or port forwarding enabled.
+  #
+  # Note: If your existing network is using the 192.168.50.x subnet, this default IP address
+  # should be changed. If more than one VM is running through VirtualBox, including other
+  # Vagrant machines, different subnets should be used for each.
+  #
+  config.vm.network :private_network, id: "devm01_primary", ip: "192.168.56.105"
+
+  config.vm.provider :hyperv do |v, override|
+    override.vm.network :private_network, id: "devm01_primary", ip: nil
+  end
+
+  # Drive mapping
+  #
+  # The following config.vm.synced_folder settings will map directories in your Vagrant
+  # virtual machine to directories on your local machine. Once these are mapped, any
+  # changes made to the files in these directories will affect both the local and virtual
+  # machine versions. Think of it as two different ways to access the same file. When the
+  # virtual machine is destroyed with `vagrant destroy`, your files will remain in your local
+  # environment.
+
+  # /srv/config/
+  #
+  # If a server-conf directory exists in the same directory as your Vagrantfile,
+  # a mapped directory inside the VM will be created that contains these files.
+  # This directory is currently used to maintain various config files for php and
+  # nginx as well as any pre-existing database files.
+  config.vm.synced_folder "config/", "/srv/config"
+
+  # /srv/www/
+  #
+  # If a www directory exists in the same directory as your Vagrantfile, a mapped directory
+  # inside the VM will be created that acts as the default location for nginx sites. Put all
+  # of your project files here that you want to access through the web server
+
+  config.vm.synced_folder "www/", "/srv/www/", :owner => "www-data", :mount_options => [ "dmode=775", "fmode=774" ]
+
+  config.vm.provision "fix-no-tty", type: "shell" do |s|
+    s.privileged = false
+    s.inline = "sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile"
+  end
+
+  # provision.sh or provision-custom.sh
+  #
+  # By default, Vagrantfile is set to use the provision.sh bash script located in the
+  # provision directory. If it is detected that a provision-custom.sh script has been
+  # created, that is run as a replacement. This is an opportunity to replace the entirety
+  # of the provisioning provided by default.
+  if File.exists?(File.join(vagrant_dir,'provision','provision-custom.sh')) then
+    config.vm.provision :shell, :path => File.join( "provision", "provision-custom.sh" )
+  else
+    config.vm.provision :shell, :path => File.join( "provision", "provision.sh" )
+  end
+
+  # Always start MySQL on boot, even when not running the full provisioner
+  # (run: "always" support added in 1.6.0)
+  config.vm.provision :shell, inline: "sudo service nginx restart", run: "always"
+end
---
+++ provision/provision.sh
@@ -0,0 +1,332 @@
+#!/bin/bash
+#
+# provision.sh
+#
+# This file is specified in Vagrantfile and is loaded by Vagrant as the primary
+# provisioning script whenever the commands `vagrant up`, `vagrant provision`,
+# or `vagrant reload` are used. It provides all of the default packages and
+# configurations included with Varying Vagrant Vagrants.
+
+# By storing the date now, we can calculate the duration of provisioning at the
+# end of this script.
+start_seconds="$(date +%s)"
+
+# PACKAGE INSTALLATION
+#
+# Build a bash array to pass all of the packages we want to install to a single
+# apt-get command. This avoids doing all the leg work each time a package is
+# set to install. It also allows us to easily comment out or add single
+# packages. We set the array as empty to begin with so that we can append
+# individual packages to it as required.
+apt_package_install_list=()
+
+# Start with a bash array containing all packages we want to install in the
+# virtual machine. We'll then loop through each of these and check individual
+# status before adding them to the apt_package_install_list array.
+apt_package_check_list=(
+    # nginx is installed as the default web server
+    nginx
+
+    # other packages that come in handy
+    imagemagick
+    git-core
+    zip
+    unzip
+    ngrep
+    curl
+    make
+
+    # ntp service to keep clock current
+    ntp
+
+    # Req'd for i18n tools
+    gettext
+)
+
+### FUNCTIONS
+
+network_detection() {
+  # Network Detection
+  #
+  # Make an HTTP request to google.com to determine if outside access is available
+  # to us. If 3 attempts with a timeout of 5 seconds are not successful, then we'll
+  # skip a few things further in provisioning rather than create a bunch of errors.
+  if [[ "$(wget --tries=3 --timeout=5 --spider http://google.com 2>&1 | grep 'connected')" ]]; then
+    echo "Network connection detected..."
+    ping_result="Connected"
+  else
+    echo "Network connection not detected. Unable to reach google.com..."
+    ping_result="Not Connected"
+  fi
+}
+
+network_check() {
+  network_detection
+  if [[ ! "$ping_result" == "Connected" ]]; then
+    echo -e "\nNo network connection available, skipping package installation"
+    exit 0
+  fi
+}
+
+noroot() {
+  sudo -EH -u "vagrant" "$@";
+}
+
+profile_setup() {
+  # Copy custom dotfiles and bin file for the vagrant user from local
+  cp "/srv/config/bash_profile" "/home/vagrant/.bash_profile"
+  # cp "/srv/config/bash_aliases" "/home/vagrant/.bash_aliases"
+  # cp "/srv/config/vimrc" "/home/vagrant/.vimrc"
+  #
+  # if [[ ! -d "/home/vagrant/.subversion" ]]; then
+  #   mkdir "/home/vagrant/.subversion"
+  # fi
+  #
+  # cp "/srv/config/subversion-servers" "/home/vagrant/.subversion/servers"
+
+  if [[ ! -d "/home/vagrant/bin" ]]; then
+    mkdir "/home/vagrant/bin"
+  fi
+
+  rsync -rvzh --delete "/srv/config/homebin/" "/home/vagrant/bin/"
+
+  echo " * Copied /srv/config/bash_profile                      to /home/vagrant/.bash_profile"
+  # echo " * Copied /srv/config/bash_aliases                      to /home/vagrant/.bash_aliases"
+  # echo " * Copied /srv/config/vimrc                             to /home/vagrant/.vimrc"
+  # echo " * Copied /srv/config/subversion-servers                to /home/vagrant/.subversion/servers"
+  # echo " * rsync'd /srv/config/homebin                          to /home/vagrant/bin"
+
+  # If a bash_prompt file exists in the VVV config/ directory, copy to the VM.
+  if [[ -f "/srv/config/bash_prompt" ]]; then
+    cp "/srv/config/bash_prompt" "/home/vagrant/.bash_prompt"
+    echo " * Copied /srv/config/bash_prompt to /home/vagrant/.bash_prompt"
+  fi
+}
+
+package_check() {
+  # Loop through each of our packages that should be installed on the system. If
+  # not yet installed, it should be added to the array of packages to install.
+  local pkg
+  local package_version
+
+  for pkg in "${apt_package_check_list[@]}"; do
+    package_version=$(dpkg -s "${pkg}" 2>&1 | grep 'Version:' | cut -d " " -f 2)
+    if [[ -n "${package_version}" ]]; then
+      space_count="$(expr 20 - "${#pkg}")" #11
+      pack_space_count="$(expr 30 - "${#package_version}")"
+      real_space="$(expr ${space_count} + ${pack_space_count} + ${#package_version})"
+      printf " * $pkg %${real_space}.${#package_version}s ${package_version}\n"
+    else
+      echo " *" $pkg [not installed]
+      apt_package_install_list+=($pkg)
+    fi
+  done
+}
+
+package_install() {
+  package_check
+
+  # MySQL
+  #
+  # Use debconf-set-selections to specify the default password for the root MySQL
+  # account. This runs on every provision, even if MySQL has been installed. If
+  # MySQL is already installed, it will not affect anything.
+  # echo mysql-server mysql-server/root_password password "root" | debconf-set-selections
+  # echo mysql-server mysql-server/root_password_again password "root" | debconf-set-selections
+
+  # Postfix
+  #
+  # Use debconf-set-selections to specify the selections in the postfix setup. Set
+  # up as an 'Internet Site' with the host name 'vvv'. Note that if your current
+  # Internet connection does not allow communication over port 25, you will not be
+  # able to send mail, even with postfix installed.
+  # echo postfix postfix/main_mailer_type select Internet Site | debconf-set-selections
+  # echo postfix postfix/mailname string vvv | debconf-set-selections
+
+  # Disable ipv6 as some ISPs/mail servers have problems with it
+  # echo "inet_protocols = ipv4" >> "/etc/postfix/main.cf"
+
+  # Provide our custom apt sources before running `apt-get update`
+  # ln -sf /srv/config/apt-source-append.list /etc/apt/sources.list.d/vvv-sources.list
+  # echo "Linked custom apt sources"
+
+  if [[ ${#apt_package_install_list[@]} = 0 ]]; then
+    echo -e "No apt packages to install.\n"
+  else
+    # Before running `apt-get update`, we should add the public keys for
+    # the packages that we are installing from non standard sources via
+    # our appended apt source.list
+
+    # Retrieve the Nginx signing key from nginx.org
+    echo "Applying Nginx signing key..."
+    wget --quiet "http://nginx.org/keys/nginx_signing.key" -O- | apt-key add -
+
+    # Apply the nodejs assigning key
+    apt-key adv --quiet --keyserver "hkp://keyserver.ubuntu.com:80" --recv-key C7917B12 2>&1 | grep "gpg:"
+    apt-key export C7917B12 | apt-key add -
+
+    # Update all of the package references before installing anything
+    echo "Running apt-get update..."
+    apt-get update -y
+
+    # Install required packages
+    echo "Installing apt-get packages..."
+    apt-get install -y ${apt_package_install_list[@]}
+
+    # Clean up apt caches
+    apt-get clean
+  fi
+}
+
+tools_install() {
+  # npm
+  #
+  # Make sure we have the latest npm version and the update checker module
+  # npm install -g npm
+  # npm install -g npm-check-updates
+
+  # xdebug
+  #
+  # XDebug 2.2.3 is provided with the Ubuntu install by default. The PECL
+  # installation allows us to use a later version. Not specifying a version
+  # will load the latest stable.
+  # pecl install xdebug
+
+  # # ack-grep
+  # #
+  # # Install ack-rep directory from the version hosted at beyondgrep.com as the
+  # # PPAs for Ubuntu Precise are not available yet.
+  # if [[ -f /usr/bin/ack ]]; then
+  #   echo "ack-grep already installed"
+  # else
+  #   echo "Installing ack-grep as ack"
+  #   curl -s http://beyondgrep.com/ack-2.14-single-file > "/usr/bin/ack" && chmod +x "/usr/bin/ack"
+  # fi
+
+  # COMPOSER
+  #
+  # Install Composer if it is not yet available.
+  if [[ ! -n "$(composer --version --no-ansi | grep 'Composer version')" ]]; then
+    echo "Installing Composer..."
+    curl -sS "https://getcomposer.org/installer" | php
+    chmod +x "composer.phar"
+    mv "composer.phar" "/usr/local/bin/composer"
+  fi
+
+  if [[ -f /vagrant/provision/github.token ]]; then
+    ghtoken=`cat /vagrant/provision/github.token`
+    composer config --global github-oauth.github.com $ghtoken
+    echo "Your personal GitHub token is set for Composer."
+  fi
+
+  # Update both Composer and any global packages. Updates to Composer are direct from
+  # the master branch on its GitHub repository.
+  if [[ -n "$(composer --version --no-ansi | grep 'Composer version')" ]]; then
+    echo "Updating Composer..."
+    COMPOSER_HOME=/usr/local/src/composer composer self-update
+    # COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update phpunit/phpunit:4.8.*
+    # COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update phpunit/php-invoker:1.1.*
+    # COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update mockery/mockery:0.9.*
+    # COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update d11wtq/boris:v1.0.8
+    COMPOSER_HOME=/usr/local/src/composer composer -q global config bin-dir /usr/local/bin
+    COMPOSER_HOME=/usr/local/src/composer composer global update
+  fi
+
+  # Grunt
+  #
+  # Install or Update Grunt based on current state.  Updates are direct
+  # from NPM
+  # if [[ "$(grunt --version)" ]]; then
+  #   echo "Updating Grunt CLI"
+  #   npm update -g grunt-cli &>/dev/null
+  #   npm update -g grunt-sass &>/dev/null
+  #   npm update -g grunt-cssjanus &>/dev/null
+  #   npm update -g grunt-rtlcss &>/dev/null
+  # else
+  #   echo "Installing Grunt CLI"
+  #   npm install -g grunt-cli &>/dev/null
+  #   npm install -g grunt-sass &>/dev/null
+  #   npm install -g grunt-cssjanus &>/dev/null
+  #   npm install -g grunt-rtlcss &>/dev/null
+  # fi
+}
+
+nginx_setup() {
+  # Create an SSL key and certificate for HTTPS support.
+  if [[ ! -e /etc/nginx/server.key ]]; then
+	  echo "Generate Nginx server private key..."
+	  vvvgenrsa="$(openssl genrsa -out /etc/nginx/server.key 2048 2>&1)"
+	  echo "$vvvgenrsa"
+  fi
+  if [[ ! -e /etc/nginx/server.crt ]]; then
+	  echo "Sign the certificate using the above private key..."
+	  vvvsigncert="$(openssl req -new -x509 \
+            -key /etc/nginx/server.key \
+            -out /etc/nginx/server.crt \
+            -days 3650 \
+            -subj /CN=*.devm01.dev 2>&1)"
+	  echo "$vvvsigncert"
+  fi
+
+  echo -e "\nSetup configuration files..."
+
+  # Used to ensure proper services are started on `vagrant up`
+  cp "/srv/config/init/start.conf" "/etc/init/start.conf"
+  echo " * Copied /srv/config/init/start.conf               to /etc/init/start.conf"
+
+  # Copy nginx configuration from local
+  cp "/srv/config/nginx-config/nginx.conf" "/etc/nginx/nginx.conf"
+  # cp "/srv/config/nginx-config/nginx-wp-common.conf" "/etc/nginx/nginx-wp-common.conf"
+  if [[ ! -d "/etc/nginx/custom-sites" ]]; then
+    mkdir "/etc/nginx/custom-sites/"
+  fi
+  rsync -rvzh --delete "/srv/config/nginx-config/sites/" "/etc/nginx/custom-sites/"
+
+  echo " * Copied /srv/config/nginx-config/nginx.conf           to /etc/nginx/nginx.conf"
+  echo " * Rsync'd /srv/config/nginx-config/sites/              to /etc/nginx/custom-sites"
+}
+
+services_restart() {
+  # RESTART SERVICES
+  #
+  # Make sure the services we expect to be running are running.
+  echo -e "\nRestart services..."
+  service nginx restart
+
+  # Disable PHP Xdebug module by default
+  # php5dismod xdebug
+
+  # Enable PHP mcrypt module by default
+  # php5enmod mcrypt
+
+  # service php5-fpm restart
+
+  # Add the vagrant user to the www-data group so that it has better access
+  # to PHP and Nginx related files.
+  usermod -a -G www-data vagrant
+}
+
+# SCRIPT
+
+network_check
+# Profile_setup
+echo "Bash profile setup and directories."
+profile_setup
+
+network_check
+# Package and Tools Install
+echo " "
+echo "Main packages check and install."
+package_install
+tools_install
+nginx_setup
+services_restart
+
+network_check
+
+#set +xv
+# And it's done
+end_seconds="$(date +%s)"
+echo "-----------------------------"
+echo "Provisioning complete in "$((${end_seconds} - ${start_seconds}))" seconds"
+echo "For further setup instructions, visit http://devm01.dev"
